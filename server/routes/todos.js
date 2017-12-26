251998efd54a1a6e7be931edd1a720a3aa57658a
Eduardo Lévano <eslevano@hotmail.com>
2017-12-26 18:07:28 -0500

    usual

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -7,7 +7,7 @@ const _ = require('lodash');
 
 router.get('/', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send(todos);
   }, (e) => {
     res.status(400).send(e);
   });
